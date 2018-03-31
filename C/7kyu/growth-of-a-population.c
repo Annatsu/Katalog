dad6b8785871d45ab2299d91e6a62d30f25b56b1
Brendon Pagano <brendonpagano@gmail.com>
2018-03-31 19:10:13 -0300

    First C just for fun

---
+++ C/7kyu/growth-of-a-population.c
@@ -0,0 +1,14 @@
+// Kata link: https://www.codewars.com/kata/growth-of-a-population
+
+int nbYear(int population, double percent, int aug, int goal) {
+    double parsedPercent = percent / 100;
+    int years = 0;
+
+    while (population < goal)
+    {
+        population += ((int)population * parsedPercent) + aug;
+        years++;
+    }
+
+    return years;
+}
